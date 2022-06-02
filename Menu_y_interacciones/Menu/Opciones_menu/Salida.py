0846e95a0defa095eb0442a14acfee176d089b84
Alexx099 <alexpildain23@gmail.com>
2022-06-02 16:11:43 -0300

    Analisis de resultados completo En ganador

--- Menu_y_interacciones/Menu/Opciones_menu/Salida.py
+++ Menu_y_interacciones/Menu/Opciones_menu/Salida.py
@@ -7,7 +7,7 @@ def salir (Victorias_jugador, Partidas_jugadas, Racha_Croupier_max, Racha_Croupi
     Porcentaje_victorias = (Victorias_jugador * 100 ) // Partidas_jugadas
         pass
     print('El porcentaje de victorias del jugador es: ',Porcentaje_victorias)
-    print(pass)
+    print(pass
 
 
 
