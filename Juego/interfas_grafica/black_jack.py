8b181df911aa0fa791c6d3dfd248b27e9ca3e769
Akeno-Senpai <milenlusianzoff@gmail.com>
2022-06-02 15:09:49 -0300

    Menu de opciones listo

--- Juego/interfas_grafica/black_jack.py
+++ Juego/interfas_grafica/black_jack.py
@@ -1,3 +1,10 @@
 from Menu_y_interacciones.Inicio.bienvenida import datos_jugador
+from Menu_y_interacciones.Menu.menu_opciones import menu_opciones
 
-datos_jugador()
+monto_inicial_pozo, nombre_jugador, apellido_jugador = datos_jugador()
+
+#apostar(monto_inicial_pozo)#
+
+#pozo_apuesta = apostar()#
+
+#apostar(pozo_apuesta)#
--- Menu_y_interacciones/Menu/menu_opciones.py
+++ Menu_y_interacciones/Menu/menu_opciones.py
@@ -1,5 +1,25 @@
 #Menu de opciones#
-
 def menu_opciones():
+    import time
     opcion = -1
     while opcion != 0:
+        print("Menu de opciones")
+        print("\n1- Apostar")
+        print("\n2- Jugar una mano")
+        print("\n3- Salir")
+        opcion = int(input("\nIngrese la opcion que quiere realizar: "))
+
+        if opcion == 1:
+            apostar()
+        elif opcion == 2:
+            jugar_mano()
+        elif opcion == 3:
+            salir()
+        else:
+            print("\nLea bien las opciones del menu. [Valor ingresado no valido]")
+            time.sleep(1.5)
+            print("Precione enter para volver al menu: ")
+            input()
+
+
+menu_opciones()
