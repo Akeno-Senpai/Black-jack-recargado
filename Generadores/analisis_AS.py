3be834f209749a9d814e285fdf0eb21b3b614500
EmSa46 <sadiremilio@gmail.com>
2022-06-03 19:50:10 -0300

    Cornetas care verga

---
+++ Generadores/analisis_AS.py
@@ -0,0 +1,6 @@
+def analisis_AS (puntaje_actual_jugador, carta_jugador):
+
+    if puntaje_actual_jugador >= 11 and carta_jugador == 11:
+        carta_jugador = 1
+
+    return carta_jugador
--- Juego/Cuerpo_del_juego/Analisis_resultados/Resultados_ronda.py
+++ Juego/Cuerpo_del_juego/Analisis_resultados/Resultados_ronda.py
@@ -1,6 +1,6 @@
 
 
-def Resultado(Puntaje_Total_jugador,Puntaje_Total_Croupier):
+def Resultado(Puntaje_Total_jugador,Puntaje_Total_Croupier,monto_apostado,monto_pozo_actual):
     if (Puntaje_Total_jugador <= 21) and (Puntaje_Total_Croupier < Puntaje_Total_jugador) or (Puntaje_Total_jugador <= 21 and Puntaje_Total_Croupier > 21):
         validacion = 0
 
@@ -16,4 +16,16 @@ def Resultado(Puntaje_Total_jugador,Puntaje_Total_Croupier):
 
         print ('\nHa ocurrido un empate', '\nTu puntaje es: ',Puntaje_Total_jugador,'\ny el del Croupier es: ',Puntaje_Total_Croupier)
 
-    return validacion
+    if validacion == 0:
+        monto_pozo_actual += monto_apostado * 2
+
+    elif validacion == 1
+        monto_pozo_actual -= monto_apostado
+
+    elif validacion == 2
+        monto_pozo_actual = monto_pozo_actual
+
+
+    return validacion, monto_pozo_actual
+
+
